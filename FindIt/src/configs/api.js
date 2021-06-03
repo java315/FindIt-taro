affaa5a5d6f7ec06d16118d8c9d36986588a965e
youngstudent2 <644699280@qq.com>
2021-06-03 17:12:47 +0800

    封装request

---
+++ FindIt/src/configs/api.js
@@ -0,0 +1,3 @@
+export const ITEM_LIST_GET = '/items'   // 物品列表
+export const ITEM_LIST_POST = '/items'  // 创建物品
+export const ITEM_GET = '/items/'
---
+++ FindIt/src/configs/config.js
@@ -0,0 +1,2 @@
+export const appApiPrefix = 'http://localhost:8089/api'
+export const h5ApiPrefix = '/api'
