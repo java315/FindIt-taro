3715c97c8dba6ed57b0c147ed77cdbcf0d29107a
youngstudent2 <644699280@qq.com>
2021-06-06 22:11:45 +0800

    创建用于api测试的页面

---
+++ FindIt/src/pages/apiTest/apiTest.vue
@@ -0,0 +1,43 @@
+<template>
+  <view class="apiTest">
+    <view class="index">
+    <AtNoticebar marquee>
+      此页面用于测试api
+    </AtNoticebar>
+    <AtButton
+      type="primary"
+      :onClick="handleClick"
+    >
+      执行
+    </AtButton>
+  </view>
+  </view>
+</template>
+
+<script>
+import findItApi from "../../utils/finditapi"
+// 按需引入, 更小的应用体积
+import { AtButton, AtNoticebar } from 'taro-ui-vue'
+import "taro-ui-vue/dist/style/components/button.scss"
+import "taro-ui-vue/dist/style/components/noticebar.scss"
+import './apiTest.less' 
+export default {
+    components: {
+    AtButton,
+    AtNoticebar
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleClick () {
+      findItApi.itemList(0).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.error(err)
+      })
+    },
+  },
+}
+</script>
